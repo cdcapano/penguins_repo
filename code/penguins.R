a39565b285b81b49a2866bfd9bccc82c1e96faf6
Collin Capano <cdcapano@syr.edu>
2025-01-23 11:56:15 -0500

    New project; add combined data

---
+++ code/penguins.R
@@ -0,0 +1,6 @@
+library(tidyverse)
+chinstrap_data <- read_csv("data/chinstrap_data.csv")
+adelie_data <- read_csv("data/adelie_data.csv")
+gentoo_data <- read_csv("data/gentoo_data.csv")
+penguin_data <- bind_rows(adelie_data, chinstrap_data, gentoo_data)
+write_csv(penguin_data, "data/penguin_data.csv")
